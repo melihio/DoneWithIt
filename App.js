51bd1b93420f53eb1e92d90536f3de4773b558f5
melihio <melihcokan@icloud.com>
2023-10-14 21:59:27 +0300

    Add ListDetails

--- App.js
+++ App.js
@@ -1,24 +1,9 @@
 import { View } from "react-native";
 import AppCard from "./app/components/Card";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ListDetails from "./app/screens/ListDetails";
 
 export default function App() {
   // return <WelcomeScreen />;
-  return (
-    <View
-      style={{
-        flex: 1,
-        padding: 10,
-        backgroundColor: "#f8f4f4",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <AppCard
-        title="Title"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ListDetails image={require("./app/assets/jacket.jpg")} />;
 }
---
+++ app/components/ListItem/ListItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Image } from "react-native";
+import AppText from "../AppText";
+import styles from "./styles";
+
+function ListItem({ title, subTitle, image }) {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={image} />
+      <View>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subTitle}>{subTitle}</AppText>
+      </View>
+    </View>
+  );
+}
+
+export default ListItem;
