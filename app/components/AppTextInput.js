b8be47e56f520fe92e46a10aa6a7e42bef5b569e
melihio <melihcokan@icloud.com>
2023-10-21 00:41:17 +0300

    Refactoring the code

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Platform, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { TextInput } from "react-native-gesture-handler";
-import colors from "../config/colors";
+import defaultStyles from "../config/styles";
 
 function AppTextInput({ icon, ...otherProps }) {
   return (
@@ -12,17 +12,17 @@ function AppTextInput({ icon, ...otherProps }) {
           style={styles.icon}
           name={icon}
           size={35}
-          color={colors.medium}
+          color={defaultStyles.colors.medium}
         />
       )}
-      <TextInput style={styles.textInput} {...otherProps} />
+      <TextInput style={defaultStyles.text} {...otherProps} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.light,
+    backgroundColor: defaultStyles.colors.light,
     borderRadius: 25,
     flexDirection: "row",
     width: "100%",
@@ -31,11 +31,6 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginRight: 10,
-  },
-  textInput: {
-    color: colors.dark,
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
   },
 });
 
