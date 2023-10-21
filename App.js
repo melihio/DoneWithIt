4c2d148508103123a93ce7dcaa45d291d1cf2d33
melihio <melihcokan@icloud.com>
2023-10-21 22:26:56 +0300

    Add ListingEditScreen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import RegisterScreen from "./app/screens/RegisterScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
   return (
     <GestureHandlerRootView>
-      <RegisterScreen />
+      <ListingEditScreen />
     </GestureHandlerRootView>
   );
 }
---
+++ app/screens/ListingEditScreen.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Screen from "../components/Screen";
+import { StyleSheet } from "react-native";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
+import * as Yup from "yup";
+import AppPicker from "../components/AppPicker";
+import AppFormPicker from "../components/forms/AppFormPicker";
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required().min(5).label("Title"),
+  category: Yup.string().required().label("Category"),
+  price: Yup.number().required().min(1).label("Price"),
+  description: Yup.string().required().min(5).label("Description"),
+});
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Camera", value: 3 },
+];
+
+function ListingEditScreen(props) {
+  return (
+    <Screen style={styles.screen}>
+      <AppForm
+        initialValues={{ title: "", category: "", price: "", description: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField autoCorrect={false} name="title" placeholder="Title" />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          name="price"
+          placeholder="Price"
+        />
+        <AppFormPicker
+          items={categories}
+          name="category"
+          placeholder="Category"
+        />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          name="description"
+          placeholder="Description"
+        />
+        <SubmitButton title="REGISTER" />
+      </AppForm>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    margin: 10,
+  },
+});
+
+export default ListingEditScreen;
